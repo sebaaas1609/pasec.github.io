725a69da1736b4bff3c4bfa89a4eb141b4a36805
Sebastian Granizo <sebitas1609gf@gmail.com>
2022-07-10 15:55:45 -0500

    first commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "El parque nacional Yasuní, en qué provincia está ubicado?",
+    answer: "Orellana",
+    options: [
+      "Morona Santiago",
+      "Sucumbios",
+      "Orellana",
+      "Pastaza"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Cual es el lugar mas frio de la tierra?",
+    answer: "Antartida",
+    options: [
+      "Oceania",
+      "Antartida",
+      "Monte Everest",
+      "Siberia"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quien escribio la Odisea?",
+    answer: "Homero",
+    options: [
+      "Dante",
+      "Hercules",
+      "Perseo",
+      "Homero"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Cual es el rio mas largo del mundo?",
+    answer: "Amazonas",
+    options: [
+      "Nilo",
+      "Amazonas",
+      "Tamesis",
+      "Mississippi"
+    ]
+  },
+    {
+    numb: 5,
+    question: "En donde se originaron los juegos olimpicos?",
+    answer: "Grecia",
+    options: [
+      "Grecia",
+      "Italia",
+      "Alemania",
+      "Turquia"
+    ]
+  },
+];
